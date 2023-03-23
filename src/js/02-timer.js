4d5cc0395e58783d833c194cd7c6101de31a0cde
Wiktoria Spychała <wiktoria_spychala@o2.pl>
2023-03-23 20:25:30 +0100

    update task 2.

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,28 +1,27 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const input = document.querySelector('input');
+const input = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
 const timerDays = document.querySelector('.value[data-days]');
 const timerHours = document.querySelector('.value[data-hours]');
-const timerMinutes = document.querySelector('.value[data=minute]');
+const timerMinutes = document.querySelector('.value[data-minutes]');
 const timerSeconds = document.querySelector('.value[data-seconds]');
 let timer = null;
-let date = new Date();
+let date = new Date().getTime();
 let selectedTime;
-
-
-btnStart.setAttribute('disabled', '');
 
 
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  defaultDate: date,
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= date.getTime()) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] <= date) {
       window.alert('Please choose a date in the future');
+      btnStart.setAttribute('disabled', '');
     } else {
       btnStart.removeAttribute('disabled');
       selectedTime = selectedDates[0];
@@ -31,6 +30,7 @@ const options = {
   },
 };
 
+flatpickr(input, options);
 
 function convertMs(ms) {
 // Number of milliseconds per unit of time
@@ -49,4 +49,23 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
+
+
+const addLeadingZero = value => {
+  return value.toString().padStart(2, '0');
+};
+
+function countingTime() {
+  const timerObj = convertMs(selectedTime - date);
+  timerDays.innerHTML = addLeadingZero(timerObj.days);
+  timerHours.innerHTML = addLeadingZero(timerObj.hours);
+  timerMinutes.innerHTML = addLeadingZero(timerObj.minutes);
+  timerSeconds.innerHTML = addLeadingZero(timerObj.seconds);
+};
+btnStart.addEventListener('click', () => {
+  btnStart.setAttribute('disabled', '');
+  timer = setInterval(countingTime(), 1000);
+  
+});
+
